4bf47d026cbac0a76a3454c80b8e8f342329ac69
Dan Octavian <danoctavian91@gmail.com>
2020-06-26 17:10:46 +0800

    add ANNUALIZED_MIN_DAYS and fix bind callback issues

--- src/index.js
+++ src/index.js
@@ -31,22 +31,32 @@ async function init () {
   const overallStatsSyncInterval = getEnv('OVERALL_STATS_SYNC_INTERVAL');
   const stakerDataSyncInterval = getEnv('STAKER_DATA_SYNC_INTERVAL');
   const syncFailureRetryInterval = getEnv('SYNC_FAILURE_INTERVAL');
+  const annualizedMinDays = getEnv('ANNUALIZED_MIN_DAYS');
   const chainName = getEnv('CHAIN_NAME', 'mainnet');
 
   const versionData = new VersionData(chainName, versionDataURL, providerURL);
-
   await versionData.init();
 
-  const chainDataAggregator = new ChainDataAggregator(versionData);
+  const chainDataAggregator = new ChainDataAggregator(versionData, annualizedMinDays);
   const app = routes(chainDataAggregator);
   await startServer(app, PORT);
   log.info(`Chain-api listening on port ${PORT}`);
   log.info(`Launching regular data sync processes..`);
 
-  runForever(chainDataAggregator.syncOverallAggregateStats, overallStatsSyncInterval, syncFailureRetryInterval, 0);
+  runForever(
+    chainDataAggregator.syncOverallAggregateStats.bind(chainDataAggregator),
+    overallStatsSyncInterval,
+    syncFailureRetryInterval,
+    0
+  );
 
   const syncDailyDelay = 10000;
-  runForever(chainDataAggregator.syncDailyStakerData, stakerDataSyncInterval, syncFailureRetryInterval, syncDailyDelay);
+  runForever(
+    chainDataAggregator.syncDailyStakerData.bind(chainDataAggregator),
+    stakerDataSyncInterval,
+    syncFailureRetryInterval,
+    syncDailyDelay
+  );
 }
 
 init()
