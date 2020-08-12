a305a7a25aedc1da4bd817f05ab65925656f3c07
Dan Octavian <danoctavian91@gmail.com>
2020-08-12 21:37:29 +0800

    add continuation id logging

--- src/log.js
+++ src/log.js
@@ -1,10 +1,39 @@
 const winston = require('winston');
 
+const { getNamespace, createNamespace } = require('cls-hooked');
+const uuidv4 = require('uuid/v4');
+
+const LOGGING_NAMESPACE_NAME = 'chain-data-api-log';
+const CONTINUATION_ID_VAR_NAME = 'continuationId';
+
+let loggingSession = getNamespace(LOGGING_NAMESPACE_NAME);
+if (!loggingSession) {
+  loggingSession = createNamespace(LOGGING_NAMESPACE_NAME);
+}
+
+function runWithContinuationId(value, continuation) {
+  if (!value) {
+    value = uuidv4();
+  }
+  let returnValue;
+  loggingSession.run(() => {
+    loggingSession.set(CONTINUATION_ID_VAR_NAME, value);
+    returnValue = continuation();
+  });
+  return returnValue;
+}
+
+const continuationIdFormat = winston.format((info) => {
+  const continuationId = loggingSession.get(CONTINUATION_ID_VAR_NAME)
+  return {...info, continuationId };
+});
+
+
 const logger = winston.createLogger({
-  format: winston.format.combine(winston.format.simple(), winston.format.timestamp()),
+  format: winston.format.combine(winston.format.simple(), winston.format.timestamp(), continuationIdFormat()),
   transports: [
     new winston.transports.Console({
-      format: winston.format.combine(winston.format.simple(), winston.format.timestamp()),
+      format: winston.format.combine(winston.format.simple(), winston.format.timestamp(), continuationIdFormat()),
       level: 'info',
       handleExceptions: true,
     }),
@@ -12,4 +41,5 @@ const logger = winston.createLogger({
   exitOnError: false,
 });
 
+logger.runWithContinuationId = runWithContinuationId;
 module.exports = logger;
